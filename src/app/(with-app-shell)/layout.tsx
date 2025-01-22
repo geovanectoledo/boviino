3a3df7e4d420110077f3cbe782633521ad1dba04
christopherkindl <53372002+christopherkindl@users.noreply.github.com>
2025-01-22 17:28:38 +0100

    add: navbar, github social

--- src/app/(with-app-shell)/layout.tsx
+++ src/app/(with-app-shell)/layout.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren } from 'react';
 
 import Footer from '../ui/footer';
+import { SiteHeader } from '../ui/site-header';
 
 export default function MarketingLayout({ children }: PropsWithChildren) {
   return (
     <>
+      <SiteHeader />
       {children}
       <Footer />
     </>
